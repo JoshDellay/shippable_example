6886869d1943cdc34838ca1c41f03c9ef8af87f4
Josh Dellay <josh.dellay@gmail.com>
2015-02-07 14:44:42 -0800

    Add example test

--- test/test.js
+++ test/test.js
@@ -1,1 +1,10 @@
 //Tests go here...
+if (!(typeof MochaWeb === 'undefined')){
+    MochaWeb.testOnly(function(){
+
+        describe('Intro', function(){
+            it('Test example 1');
+        });
+
+    });
+}
