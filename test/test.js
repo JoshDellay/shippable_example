1d4dc102652068e99a039282740e274d9cd5fb42
Josh Dellay <josh.dellay@gmail.com>
2015-02-07 15:06:48 -0800

    Add proper test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
-//Tests go here...
+
 if (!(typeof MochaWeb === 'undefined')){
     MochaWeb.testOnly(function(){
 
         describe('Intro', function(){
-            it('Test example 1');
+            it('Test example 1', function(){
+                chai.assert.equal(1, 1);
+            });
         });
 
     });
