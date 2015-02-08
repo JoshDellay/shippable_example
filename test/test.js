d096aee35fcf380fbc9673885a0a8d07b6c841a4
Josh Dellay <josh.dellay@gmail.com>
2015-02-07 22:36:43 -0800

    Fix casper

--- test/test.js
+++ test/test.js
@@ -1,12 +1,18 @@
+var url = '127.0.0.1:3000';
+var TestObject = {
+    key: true
+};
 
-if (!(typeof MochaWeb === 'undefined')){
-    MochaWeb.testOnly(function(){
-
-        describe('Intro', function(){
-            it('Test example 1', function(){
-                chai.assert.equal(1, 1);
-            });
-        });
-
+describe("A mocha test", function() {
+    before(function() {
+        casper.start(url);
     });
-}
+    it("should have a global namespace", function() {
+        casper.then(function () {
+            var evalResult = casper.evaluate(function() {
+                return true;
+            });
+            evalResult.should.equal(true);
+        });
+    });
+});
