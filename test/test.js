1ab67abd656666ce550ccc8eccd23fb7fc342b1c
Josh <josh.dellay@gmail.com>
2015-02-09 00:29:44 -0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,10 +1,9 @@
-var url = '127.0.0.1:3000';
-var TestObject = {
-    key: true
-};
-
-describe("A mocha test", function() {
-    it("should have a global namespace", function(){
-        chai.assert.equal(1,1);
+if (!(typeof MochaWeb === 'undefined')){
+  MochaWeb.testOnly(function(){
+    describe("a group of tests", function(){
+      it("should respect equality", function(){
+        chai.assert.equal(5,5);
+      });
     });
-});
+  });
+}
