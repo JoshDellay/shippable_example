1529035e38eb7af61358998646cf341f61a7d3de
Josh <josh.dellay@gmail.com>
2015-02-09 16:26:58 -0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,20 +1,13 @@
 var url = '127.0.0.1:3000';
 
 describe("Example Tests", function() {
-  before(function() {
-    casper.start(url);
-    casper.on('Test1', function(message) {
-      this.echo(message);
+    it("should equal true", function() {
+        casper.then(function () {
+        var evalResult = casper.evaluate(function() {
+            return {
+              "testResults": true,
+            }
+        });
+        evalResult.testResults.should.equal(true);
     });
-  });
-  it("should equal true", function() {
-    casper.then(function () {
-      var evalResult = casper.evaluate(function() {
-        return {
-          "true": true,
-        }
-      });
-      evalResult.true.should.equal(true);
-    });
-  });
 });
