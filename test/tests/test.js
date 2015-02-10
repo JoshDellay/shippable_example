15fda1ec9c1767857f4480ca25caa69e075c0723
Josh Dellay <josh.dellay@gmail.com>
2015-02-09 16:32:11 -0800

    fix test structure

---
+++ test/tests/test.js
@@ -0,0 +1,13 @@
+var url = '127.0.0.1:3000';
+
+describe("Example Tests", function() {
+    it("should equal true", function() {
+        casper.then(function () {
+        var evalResult = casper.evaluate(function() {
+            return {
+              "testResults": true,
+            }
+        });
+        evalResult.testResults.should.equal(true);
+    });
+});
