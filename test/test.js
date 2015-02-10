adf56438396d1fb7698b2e0d3d46846686807fe0
Josh <josh.dellay@gmail.com>
2015-02-09 16:41:41 -0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,12 @@
 var url = '127.0.0.1:3000';
 
 describe("Example Tests", function() {
+    before(function() {
+        casper.start(url);
+        casper.on('remote.message', function(message) {
+          this.echo(message);
+        });
+    });
     it("should equal true", function() {
         casper.then(function () {
         var evalResult = casper.evaluate(function() {
