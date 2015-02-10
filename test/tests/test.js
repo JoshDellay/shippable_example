477598afc8626feeaf636b2bd55e294de68ca7a8
Josh Dellay <josh.dellay@gmail.com>
2015-02-09 16:51:22 -0800

    fix test

--- test/tests/test.js
+++ test/tests/test.js
@@ -3,11 +3,13 @@ var url = '127.0.0.1:3000';
 describe("Example Tests", function() {
     it("should equal true", function() {
         casper.then(function () {
-        var evalResult = casper.evaluate(function() {
-            return {
-              "testResults": true,
-            }
+            var evalResult = casper.evaluate(function() {
+                return {
+                  "testResults": true,
+                }
+            });
+
+            evalResult.testResults.should.equal(true);
         });
-        evalResult.testResults.should.equal(true);
     });
 });
